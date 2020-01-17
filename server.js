725480cff67acac2914fb6d99e9de37dacc673c7
gauravkumar07nagarro <gaurav.kumar07@nagarro.com>
2020-01-17 11:54:50 +0530

    changes

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express')
 
 class Server {
     constructor() {
+        this.port = 3001
+        this.host = "localhost"
+        this.app = express()
+        this.http = http.Server(this.app)
+    }
 
+    appExecute() {
+        this.http.listen(this.port, this.host, () => {
+            console.log(`Listening on http://${this.host}:${this.port}`);
+        });
     }
 }
+
+const serv = new Server();
+serv.appExecute()
 
